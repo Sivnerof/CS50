e49e915d2524bdff5aefa7642184593da79fe51b
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-08 19:59:59 -0700

    CS50 Lecture Notes, week one

---
+++ Week_One/Lecture_Notes/bricks2.c
@@ -0,0 +1,24 @@
+// Prints an n-by-n grid of bricks with a loop
+
+#include <cs50.h>
+#include <stdio.h>
+
+// TODO: use do-while first, then just use constant
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
---
+++ Week_One/Lecture_Notes/coins2.c
@@ -0,0 +1,21 @@
+// Prints a row of n question marks with a loop
+
+// TODO: add get_width() function
+//
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
