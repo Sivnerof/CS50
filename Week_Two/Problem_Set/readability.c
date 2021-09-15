ad92dc94e4dd8228f87522218a9b301e3c4dc333
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-15 16:33:08 -0700

    Finished program, applied Coleman-Liau formula, cleaned up code, added comments

--- Week_Two/Problem_Set/readability.c
+++ Week_Two/Problem_Set/readability.c
@@ -4,77 +4,73 @@
 #include<string.h>
 #include<math.h>
 
-int countLetters(string usersString);
+/*
+    A program that calculates the reading level of a users supllied text,
+    according to grade level using the Coleman-Liau formula.
+*/
+
+int colemanLiau(string usersString);
 
 int main(void)
 {
     //Get users input
     string userText = get_string("Text: ");
-    int userTextLength = countLetters(userText);
-    //printf("Grade %i\n", );
+
+    //Pass to function for calculation
+    int gradeLevel = colemanLiau(userText);
+
+    //Handles under first grade reading levels
+    if (gradeLevel < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+
+    //Handles reading levels above 16
+    else if (gradeLevel > 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+    else
+    {
+        printf("Grade %i\n", gradeLevel);
+    }
 
     return 0;
 }
 
-//Count overall length of users input
-int countLetters(string usersString)
+int colemanLiau(string usersString)
 {
     int letters = 0;
     int words = 1;
     int sentences = 0;
-    for(int i = 0, n = strlen(usersString); i < n; i++)
+
+    //Loops through all characters in users provided string
+    for (int i = 0, n = strlen(usersString); i < n; i++)
     {
-        if(isalpha(usersString[i]))
+        //Catches all alphabetical characters and increments their count
+        if (isalpha(usersString[i]))
         {
             letters++;
         }
-        else if(isspace(usersString[i]))
+
+        //Identifies all white space as the ending of a word
+        else if (isspace(usersString[i]))
         {
             words++;
         }
-        else if(usersString[i] == '!' || usersString[i] == '?' || usersString[i] == '.')
+
+        //Identifies these three as the ends of sentences
+        else if (usersString[i] == '!' || usersString[i] == '?' || usersString[i] == '.')
         {
             sentences++;
         }
     }
-    printf("letters: %i\n", letters);
-    printf("spaces: %i\n", words);
-    printf("sentences: %i\n", sentences);
 
+    //Coleman-Liau Index
     float L = (((float) letters / words) * 100);
     float S = (((float) sentences / words) * 100);
-    printf("test: %f\n", L);
-    printf("test: %f\n", S);
     int index = round(0.0588 * L - 0.296 * S - 15.8);
-    printf("index: %i", index);
 
-
-    return letters;
+    return index;
 }
-
-
-
-
-
-/*
-index = 0.0588 * L - 0.296 * S - 15.8
-Here, L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
-
-
-
-Example = Congratulations! Today is your day. You're off to Great Places! You're off and away!
-The text the user inputted has 65 letters, 4 sentences, and 14 words.
-
-
-65 letters per 14 words is an average of about 464.29 letters per 100 words.
-And 4 sentences per 14 words is an average of about 28.57 sentences per 100 words.
-Plugged into the Coleman-Liau formula, and rounded to the nearest whole number, we get an answer of 3: so this passage is at a third grade reading level.
-
-
-Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
-If the resulting index number is 16 or higher (equivalent to or greater than a senior undergraduate reading level),
-your program should output "Grade 16+" instead of giving the exact index number.
-If the index number is less than 1, your program should output "Before Grade 1".
-
-
-*/
