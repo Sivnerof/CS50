0ea2bb8a7b1a427a7947a9adb3ae804a46a57fb8
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-15 16:06:14 -0700

    Finishing up

--- Week_Two/Problem_Set/readability.c
+++ Week_Two/Problem_Set/readability.c
@@ -2,6 +2,7 @@
 #include<cs50.h>
 #include<ctype.h>
 #include<string.h>
+#include<math.h>
 
 int countLetters(string usersString);
 
@@ -10,14 +11,16 @@ int main(void)
     //Get users input
     string userText = get_string("Text: ");
     int userTextLength = countLetters(userText);
-    //printf("Length is: %i\n", userTextLength);
+    //printf("Grade %i\n", );
+
+    return 0;
 }
 
 //Count overall length of users input
 int countLetters(string usersString)
 {
     int letters = 0;
-    int spacesInBetween = 1;
+    int words = 1;
     int sentences = 0;
     for(int i = 0, n = strlen(usersString); i < n; i++)
     {
@@ -27,7 +30,7 @@ int countLetters(string usersString)
         }
         else if(isspace(usersString[i]))
         {
-            spacesInBetween++;
+            words++;
         }
         else if(usersString[i] == '!' || usersString[i] == '?' || usersString[i] == '.')
         {
@@ -35,9 +38,18 @@ int countLetters(string usersString)
         }
     }
     printf("letters: %i\n", letters);
-    printf("spaces: %i\n", spacesInBetween);
+    printf("spaces: %i\n", words);
     printf("sentences: %i\n", sentences);
-    return counter;
+
+    float L = (((float) letters / words) * 100);
+    float S = (((float) sentences / words) * 100);
+    printf("test: %f\n", L);
+    printf("test: %f\n", S);
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    printf("index: %i", index);
+
+
+    return letters;
 }
 
 
@@ -45,12 +57,15 @@ int countLetters(string usersString)
 
 
 /*
-with a . or a ! or a ? to be a sentence
 index = 0.0588 * L - 0.296 * S - 15.8
 Here, L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
 
+
+
 Example = Congratulations! Today is your day. You're off to Great Places! You're off and away!
 The text the user inputted has 65 letters, 4 sentences, and 14 words.
+
+
 65 letters per 14 words is an average of about 464.29 letters per 100 words.
 And 4 sentences per 14 words is an average of about 28.57 sentences per 100 words.
 Plugged into the Coleman-Liau formula, and rounded to the nearest whole number, we get an answer of 3: so this passage is at a third grade reading level.
