c889ad9284cdcfc4783f555c2e5c7a7c074c8e95
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-16 20:07:23 -0700

    Had to change some things around

--- Week_Two/Problem_Set/caesar.c
+++ Week_Two/Problem_Set/caesar.c
@@ -8,18 +8,18 @@
     A program that uses a Caesar Cipher on user provided plaintext where the key to be used is inputted through a command line argument.
 */
 
-string caesarCipher(int rotation, string userText);
+char rotatePlain(char individualChar, int keyUsed);
 
 int main(int argc, string argv[])
 {
-    //Handle too many arguments
+    //Handles too many arguments
     if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    //Handle non numeric arguments
+    //Handle non-numeric arguments
     for(int i = 0, n = strlen(argv[1]); i < n; i++)
     {
         if(!isdigit(argv[1][i]))
@@ -29,49 +29,33 @@ int main(int argc, string argv[])
         }
     }
 
-    //Result is a valid Key
+    //Key to be used in rotation
     int key = atoi(argv[1]);
 
-    //User Inputs
-    string plainText = get_string("plaintext: ");
+    //User provided input
+    string plainText = get_string("plaintext:  ");
 
-    //Pass To function for rotation
-    string cipherText = caesarCipher(key, plainText);
+    //Result
+    char singleRot;
+    printf("ciphertext: ");
 
-    //printf("ciphertext: \n");
+    //Individual chars sent to function
+    for(int i = 0, n = strlen(plainText); i < n; i++)
+    {
+        singleRot = rotatePlain(plainText[i], key);
+        printf("%c", singleRot);
+    }
+    printf("\n");
 
     return 0;
 }
 
-string caesarCipher(int rotation, string userText)
+//Rotate Plain Text
+char rotatePlain(char individualChar, int keyUsed)
 {
-    string testing;
-    for(int i = 0, n = strlen(userText); i < n; i++)
+    if(isalpha(individualChar))
     {
-        if(isalpha(userText[i]))
-        {
-            userText[i] += 1;
-        }
-        //printf("%c", userText[i]);
-        testing = userText;
+        individualChar += keyUsed;
     }
-    return "test";
+    return individualChar;
 }
-
-/*
-
-
-pi is the ith character in p, and k is a secret key (i.e., a non-negative integer), then each letter, ci, in the ciphertext, c, is computed as
-
-ci = (pi + k) % 26
-
-wherein % 26 here means “remainder when dividing by 26.”
-This formula perhaps makes the cipher seem more complicated than it is,
-but it’s really just a concise way of expressing the algorithm precisely.
-Indeed, for the sake of discussion,
-think of A (or a) as 0, B (or b) as 1, …, H (or h) as 7, I (or i) as 8, …, and Z (or z) as 25.
-Suppose that Caesar just wants to say Hi to someone confidentially using,
-this time, a key, k, of 3.
-And so his plaintext, p, is Hi, in which case his plaintext’s first character, p0, is H (aka 7), and his plaintext’s second character, p1, is i (aka 8).
-His ciphertext’s first character, c0, is thus K, and his ciphertext’s second character, c1, is thus L.
-
