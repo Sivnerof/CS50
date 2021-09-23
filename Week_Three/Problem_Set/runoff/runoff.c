3f56e332c110224dc54a4525a25995855f1c6052
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-22 23:48:47 -0700

    updated vote function

--- Week_Three/Problem_Set/runoff/runoff.c
+++ Week_Three/Problem_Set/runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,14 +128,34 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+
+    // Loop through all candidates
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // Catch candidate name and position in iteration
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            // Update the nested array with the location of candidate
+            preferences[voter][rank] = i;
+            return true;
+        }
+    }
+    // Handle wrong names
     return false;
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    // Loops through all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+
+        }
+    }
+
     return;
 }
 
@@ -167,41 +188,10 @@ void eliminate(int min)
 }
 
 /*
-Let’s open up runoff.c to take a look at what’s already there. We’re defining two constants: MAX_CANDIDATES for the maximum number of candidates in the election, and MAX_VOTERS for the maximum number of voters in the election.
-
-Next up is a two-dimensional array preferences. The array preferences[i] will represent all of the preferences for voter number i, and the integer preferences[i][j] here will store the index of the candidate who is the jth preference for voter i.
-
-Next up is a struct called candidate. Every candidate has a string field for their name, and int representing the number of votes they currently have, and a bool value called eliminated that indicates whether the candidate has been eliminated from the election. The array candidates will keep track of all of the candidates in the election.
-
-The program also has two global variables: voter_count and candidate_count.
-
-Now onto main. Notice that after determining the number of candidates and the number of voters, the main voting loop begins, giving every voter a chance to vote. As the voter enters their preferences, the vote function is called to keep track of all of the preferences. If at any point, the ballot is deemed to be invalid, the program exits.
-
-Once all of the votes are in, another loop begins: this one’s going to keep looping through the runoff process of checking for a winner and eliminating the last place candidate until there is a winner.
-
-The first call here is to a function called tabulate, which should look at all of the voters’ preferences and compute the current vote totals, by looking at each voter’s top choice candidate who hasn’t yet been eliminated. Next, the print_winner function should print out the winner if there is one; if there is, the program is over. But otherwise, the program needs to determine the fewest number of votes anyone still in the election received (via a call to find_min). If it turns out that everyone in the election is tied with the same number of votes (as determined by the is_tie function), the election is declared a tie; otherwise, the last-place candidate (or candidates) is eliminated from the election via a call to the eliminate function.
-
-If you look a bit further down in the file, you’ll see that these functions — vote, tabulate, print_winner, find_min, is_tie, and eliminate — are all left to up to you to complete!
-
-Complete the vote function.
-
-    The function takes arguments voter, rank, and name. If name is a match for the name of a valid candidate, then you should update the global preferences array to indicate that the voter voter has that candidate as their rank preference (where 0 is the first preference, 1 is the second preference, etc.).
-    If the preference is successfully recorded, the function should return true; the function should return false otherwise (if, for instance, name is not the name of one of the candidates).
-    You may assume that no two candidates will have the same name.
-
-Hints
-
-    Recall that candidate_count stores the number of candidates in the election.
-    Recall that you can use strcmp to compare two strings.
-    Recall that preferences[i][j] stores the index of the candidate who is the jth ranked preference for the ith voter.
-
-Complete the tabulate function.
 
     The function should update the number of votes each candidate has at this stage in the runoff.
     Recall that at each stage in the runoff, every voter effectively votes for their top-preferred candidate who has not already been eliminated.
-
 Hints
-
     Recall that voter_count stores the number of voters in the election.
     Recall that for a voter i, their top choice candidate is represented by preferences[i][0], their second choice candidate by preferences[i][1], etc.
     Recall that the candidate struct has a field called eliminated, which will be true if the candidate has been eliminated from the election.
