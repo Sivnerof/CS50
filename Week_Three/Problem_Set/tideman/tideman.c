3339723914ae0ff9647d9e04bce23d0b5249e69c
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-24 23:40:25 -0700

    finished recording preferences function

--- Week_Three/Problem_Set/tideman/tideman.c
+++ Week_Three/Problem_Set/tideman/tideman.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <string.h>
 #include <stdio.h>
 
 // Max number of candidates
@@ -99,14 +100,41 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // Catch candidates position in array
+        if (strcmp(candidates[i], name) == 0)
+        {
+            // array ranks position equal to incoming rank equals candidate location
+            ranks[rank] = i;
+            return true;
+        }
+    }
+    // Handle false entries
     return false;
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+/*             A  B  C  D  E  F          i = |   j = ----->
+              [0][1][2][3][4][5]             |
+        A  [0][0][?][?][?][?][?]             v
+        B  [1][?][0][?][?][?][?]
+        C  [2][?][?][0][?][?][?]
+        D  [3][?][?][?][0][?][?]
+        E  [4][?][?][?][?][0][?]
+        F  [5][?][?][?][?][?][0]
+*/
+
+    // Loop through voter array
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[j]]++;
+        }
+    }
     return;
 }
 
@@ -137,3 +165,26 @@ void print_winner(void)
     // TODO
     return;
 }
+
+/*
+
+Complete the add_pairs function.
+    The function should add all pairs of candidates where one candidate is preferred to the pairs array.
+    A pair of candidates who are tied (one is not preferred over the other) should not be added to the array.
+    The function should update the global variable pair_count to be the number of pairs of candidates.
+    (The pairs should thus all be stored between pairs[0] and pairs[pair_count - 1], inclusive).
+
+Complete the sort_pairs function.
+    The function should sort the pairs array in decreasing order of strength of victory,
+    where strength of victory is defined to be the number of voters who prefer the preferred candidate.
+    If multiple pairs have the same strength of victory, you may assume that the order does not matter.
+
+Complete the lock_pairs function.
+    The function should create the locked graph,
+    adding all edges in decreasing order of victory strength so long as the edge would not create a cycle.
+
+Complete the print_winner function.
+    The function should print out the name of the candidate who is the source of the graph.
+    You may assume there will not be more than one source.
+
+*/
