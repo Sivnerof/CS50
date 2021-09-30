f43761645cbe2b34527c8cbc9d88f83469e872dc
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-29 20:01:40 -0700

    Filter less finished

--- Week_4/Problem_Set/filter/helpers.c
+++ Week_4/Problem_Set/filter/helpers.c
@@ -31,11 +31,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 
             // If result is more than 8 bits worth of color cap at 255
             if (sepiaRed > 255)
+            {
                 sepiaRed = 255;
+            }
             if (sepiaGreen > 255)
+            {
                 sepiaGreen = 255;
+            }
             if (sepiaRed > 255)
+            {
                 sepiaRed = 255;
+            }
 
             // Change pixel color values to Sepia
             image[i][j].rgbtRed = sepiaRed;
@@ -49,32 +55,50 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int swapRed, swapGreen, swapBlue, center;
-    int position;
+    // Declare swap variables, and position setter, and find center
+    int swapRed, swapGreen, swapBlue, center, position;
+
     for (int i = 0; i < height; i++)
     {
+        // i, j match evenly to their respective partners
         if (width % 2 == 0)
         {
+            // Set position to last pixel
             position = width - 1;
+
+            // Set center for even pixels
             center = width / 2;
+
+            // Loop until halfway
             for (int j = 0; j < center; j++)
             {
+                // Containers for swap
                 swapRed = image[i][position].rgbtRed;
                 swapGreen = image[i][position].rgbtGreen;
                 swapBlue = image[i][position].rgbtBlue;
+
+                // Start pixels pushed to end
                 image[i][position].rgbtRed = image[i][j].rgbtRed;
                 image[i][position].rgbtGreen = image[i][j].rgbtGreen;
                 image[i][position].rgbtBlue = image[i][j].rgbtBlue;
+
+                // Container dump into start
                 image[i][j].rgbtRed = swapRed;
                 image[i][j].rgbtGreen = swapGreen;
                 image[i][j].rgbtBlue = swapBlue;
+
+                // Next position up
                 position--;
             }
         }
 
         else
         {
+
+            // Same as above
             position = width - 1;
+
+            // Except this time we refocus center for odd pixel count in row
             center = ceil(width / 2);
             for (int j = 0; j <= center; j++)
             {
@@ -97,5 +121,66 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Empty struct to be populated with blurred pixels
+    RGBTRIPLE blurredImage[height][width];
+
+    // Sums of RGB values in a pixel
+    float sumRed, sumGreen, sumBlue;
+
+    // Count to keep track of times to divide sums by
+    int count;
+
+    // Loop through pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Reset sums and count
+            count = 0;
+            sumRed = sumGreen = sumBlue = 0;
+
+            // Loop through surrounding pixels on x
+            for (int x = -1; x < 2; x++)
+            {
+                // Loop through surrounding pixels on y
+                for (int y = -1; y < 2; y++)
+                {
+                    // Set positions
+                    int xPosition = i + x;
+                    int yPosition = j + y;
+
+                    // Exclude and move to next position
+                    if (xPosition < 0 || xPosition > (height - 1) || yPosition < 0 || yPosition > (width - 1))
+                    {
+                        continue;
+                    }
+
+                    // Add up sums of individual RGB values
+                    sumRed += image[xPosition][yPosition].rgbtRed;
+                    sumGreen += image[xPosition][yPosition].rgbtGreen;
+                    sumBlue += image[xPosition][yPosition].rgbtBlue;
+
+                    // Track of times needed to divide by
+                    count++;
+                }
+
+                // Drop values into dummy struct pixel location
+                blurredImage[i][j].rgbtRed = round(sumRed / count);
+                blurredImage[i][j].rgbtGreen = round(sumGreen / count);
+                blurredImage[i][j].rgbtBlue = round(sumBlue / count);
+            }
+        }
+    }
+
+    // Blurred pixels looped into originals positions
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = blurredImage[i][j].rgbtRed;
+            image[i][j].rgbtGreen = blurredImage[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = blurredImage[i][j].rgbtBlue;
+        }
+    }
     return;
 }
