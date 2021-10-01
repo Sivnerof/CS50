c771c988caaa35eaf4c62370101909627ee0f879
Rogue Blue <rogueblueprint@protonmail.com>
2021-09-30 22:33:20 -0700

    Filter more finished

--- Week_4/Problem_Set/filter_more/helpers.c
+++ Week_4/Problem_Set/filter_more/helpers.c
@@ -153,5 +153,85 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE sobelImage[height][width];
+
+    // 3x3 Sobel Grids
+
+    // Gx
+    int sobelX[3][3] =
+    {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+
+    // Gy
+    int sobelY[3][3] =
+    {
+        {-1, -2, -1},
+        {0, 0, 0},
+        {1, 2, 1}
+    };
+
+    // Loop through pixels
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < width; j++)
+        {
+            // Set RGB variables
+            float gxRed, gxBlue, gxGreen, gyRed, gyBlue, gyGreen;
+            gxRed = gxBlue = gxGreen = gyRed = gyBlue = gyGreen = 0;
+
+            // Outer loop checks values around position in y axis
+            for (int k = -1; k < 2; k++)
+            {
+                // Inner loop does same along x axis
+                for (int l = -1; l < 2; l++)
+                {
+
+                    // Exclude corner and edge pixels from algorithm
+                    if (i + k < 0 || i + k >= height || j + l < 0 || j + l >= width)
+                    {
+                        continue;
+                    }
+
+                    // Run Sobel Grid along x axis
+                    gxRed += image[i + k][j + l].rgbtRed * sobelX[k + 1][l + 1];
+                    gxGreen += image[i + k][j + l].rgbtGreen * sobelX[k + 1][l + 1];
+                    gxBlue += image[i + k][j + l].rgbtBlue * sobelX[k + 1][l + 1];
+
+                    // Run Sobel Grid along y axis
+                    gyRed += image[i + k][j + l].rgbtRed * sobelY[k + 1][l + 1];
+                    gyGreen += image[i + k][j + l].rgbtGreen * sobelY[k + 1][l + 1];
+                    gyBlue += image[i + k][j + l].rgbtBlue * sobelY[k + 1][l + 1];
+                }
+            }
+
+            int redPixel, greenPixel, bluePixel;
+
+            // Apply second part of Sobel algorithm
+
+            // Gx^2 + Gy^2 to fit both results of Sobel Grids into one pixel
+            redPixel = round(sqrt(gxRed * gxRed + gyRed * gyRed));
+            greenPixel = round(sqrt(gxGreen * gxGreen + gyGreen * gyGreen));
+            bluePixel = round(sqrt(gxBlue * gxBlue + gyBlue * gyBlue));
+
+            // If results are greater than 255 max channel rgb value, set cap to 255
+            sobelImage[i][j].rgbtRed = redPixel > 255 ? 255 : redPixel;
+            sobelImage[i][j].rgbtGreen = greenPixel > 255 ? 255 : greenPixel;
+            sobelImage[i][j].rgbtBlue = bluePixel > 255 ? 255 : bluePixel;
+        }
+    }
+
+    // Transfer pixels to image brought in
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = sobelImage[i][j].rgbtRed;
+            image[i][j].rgbtGreen = sobelImage[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = sobelImage[i][j].rgbtBlue;
+        }
+    }
 }
