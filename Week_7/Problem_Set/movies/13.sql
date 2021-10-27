65f0b369bbf5217e34084a52d532c0164793de84
Rogue Blue <rogueblueprint@protonmail.com>
2021-10-27 00:16:52 -0700

    Finished Movies Database Problem Set

--- Week_7/Problem_Set/movies/13.sql
+++ Week_7/Problem_Set/movies/13.sql
@@ -2,3 +2,5 @@
     -- Your query should output a table with a single column for the name of each person.
     -- There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
     -- Kevin Bacon himself should not be included in the resulting list.
+
+SELECT DISTINCT (name) FROM people JOIN stars ON people.id = stars.person_id WHERE stars.movie_id IN (SELECT movie_id FROM people JOIN stars ON stars.person_id = people.id WHERE people.name = "Kevin Bacon" AND birth = 1958) AND name != "Kevin Bacon";
