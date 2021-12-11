c41a6df12444ba6dbad31058a5df8c3ee7711299
Rogue Blue <rogueblueprint@protonmail.com>
2021-12-10 20:37:42 -0700

    problems with ottendorf

--- Final_Project/ottendorf/ottendorf.py
+++ Final_Project/ottendorf/ottendorf.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import os
 
 def main():
     # Get users intentions
@@ -10,17 +11,23 @@ def main():
 
     # Get user text file
     file_name = input("Enter name of text file: ")
-    if file_name[len(file_name) - 4:] != ".txt":
+    if os.path.isfile(file_name) == False:
+        sys.exit("File name does not exist")
+    elif file_name[len(file_name) - 4:] != ".txt":
         sys.exit("File must be a .txt file")
 
     # Get users message
-    user_message = input("Enter message: ").upper()
-    
-    # Turn string into list
-    user_message = string_convert(user_message)
+    if mode == 'e':
+        user_message = input("Enter message: ").upper()
+        for i in range(len(user_message)):
+            if user_message[i].isalpha() == False and user_message[i].isspace() == False:
+                sys.exit("Message must only be alphabetical letters or spaces, no symbols or numbers.")
 
-    # Grab positions of letters in file
-    letter_coords(user_message, file_name)
+        # Turn string into list
+        user_message = string_convert(user_message)
+
+        # Grab positions of letters in file
+        letter_coords(user_message, file_name)
 
     sys.exit(0)
 
@@ -33,32 +40,25 @@ def string_convert(message):
 
 
 def letter_coords(message, file):
-    """
     test = []
-    line_position = 0
-    word_position = 0
     with open(file) as f:
         for lines in f:
-            test.append(lines.upper())
-
-    for i in range(len(test)):
-        for j in test[i]:
-            if j == ' ':
-                word_position += 1
-        line_position += 1
-    print(line_position, word_position)
-    print(test[0].index('C'))
+            if lines.isspace() == False:
+                test.append(lines.upper())
 
     testing = random.randrange(len(test))
-    print(testing)
-    print(testing)
     print(test[testing])
-    """
-    pass
+    if 'E' in test[testing]:
+        print("true") #
+        print(test[testing].index('E'))
+        print(testing)
 
-
-
-    
+    for i in range(len(message)):
+        testing = random.randrange(len(test))
+        if message[i] in test[testing]:
+            print(f"{message[i]} was found at position: {test[testing].index(message[i])} on line {testing}")
+        else:
+            print(f"{message[i]} was not found on line: {testing}")
 
 
 if __name__ == "__main__":
